fdb5ea6e8dfc010e1e267a00918adbe60b3b5ecf
martinL0918 <martin641379@gmail.com>
2019-11-06 18:43:05 +0800

    hide token

--- src/main/java/doggyBot.java
+++ src/main/java/doggyBot.java
@@ -608,8 +608,7 @@ public class doggyBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         // TODO
-        return "844161272:AAFSfCUKlWHCukJhjAjzjDze577oYcvdj3k";
-        //Online :"844161272:AAFSfCUKlWHCukJhjAjzjDze577oYcvdj3k";
-        //Test   :"1032223034:AAFcHuiTtpTcB2_9jpMw9V-TGbhsfHC-RuY";
+        return "123";
+
     }
 }
