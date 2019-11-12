4b5f9ce253e813525cdeeb7fe69e59a1a8c2c8b6
martinL0918 <martin641379@gmail.com>
2019-11-12 18:44:33 +0800

    friend system

--- src/main/java/doggyBot.java
+++ src/main/java/doggyBot.java
@@ -2435,7 +2435,7 @@ public class doggyBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         // TODO
-        return "1032223034:AAFcHuiTtpTcB2_9jpMw9V-TGbhsfHC-RuY";
+        return "844161272:AAFSfCUKlWHCukJhjAjzjDze577oYcvdj3k";
         //Online :"844161272:AAFSfCUKlWHCukJhjAjzjDze577oYcvdj3k";
         //Test   :"1032223034:AAFcHuiTtpTcB2_9jpMw9V-TGbhsfHC-RuY";
     }
